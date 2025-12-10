21c94fad1227deabd99dd2295ade310331836c6a
Sadiqul  Islam <sadiqul@joinventureai.com>
2025-12-10 17:27:59 +0600

    updated extension and package file

--- extension.js
+++ extension.js
@@ -1,24 +1,38 @@
 const vscode = require('vscode');
-const fs = require('fs-extra');
-const path = require('path');
+const { exec } = require('child_process');
+const fs = require('fs');
 
 function activate(context) {
     let disposable = vscode.commands.registerCommand('djangoDRF.scaffoldProject', async () => {
         const workspaceFolders = vscode.workspace.workspaceFolders;
+
         if (!workspaceFolders) {
             vscode.window.showErrorMessage('Please open a workspace folder first.');
             return;
         }
 
         const targetFolder = workspaceFolders[0].uri.fsPath;
-        const sourceFolder = path.join(__dirname, 'template');
 
-        try {
-            await fs.copy(sourceFolder, targetFolder, { overwrite: true });
-            vscode.window.showInformationMessage('Django + DRF project scaffolded successfully!');
-        } catch (err) {
-            vscode.window.showErrorMessage('Error scaffolding project: ' + err.message);
+        // Check if folder is empty
+        const files = fs.readdirSync(targetFolder);
+        if (files.length > 0) {
+            vscode.window.showWarningMessage('Target folder is not empty!');
+            return;
         }
+
+        vscode.window.showInformationMessage('Cloning django-template from GitHub...');
+
+        // Clone directly from django-template repo
+        const repoUrl = 'https://github.com/devsadiqul/django-template.git';
+        exec(`git clone ${repoUrl} "${targetFolder}"`, (err, stdout, stderr) => {
+            if (err) {
+                vscode.window.showErrorMessage('Git clone failed: ' + err.message);
+                console.error(stderr);
+            } else {
+                vscode.window.showInformationMessage('Django DRF project scaffolded successfully from django-template!');
+                console.log(stdout);
+            }
+        });
     });
 
     context.subscriptions.push(disposable);
@@ -26,7 +40,4 @@ function activate(context) {
 
 function deactivate() {}
 
-module.exports = {
-    activate,
-    deactivate
-};
+module.exports = { activate, deactivate };
